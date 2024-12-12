8ce3c2e90cff8baa8391da48a584ccda67beeba8
caioyuri99 <caioyuri1000@gmail.com>
2024-12-12 14:54:53 -0300

    create dedent function to keep indentation consistent in tests archives

--- src/modules/utils/utils.module.ts
+++ src/modules/utils/utils.module.ts
@@ -30,3 +30,30 @@ export function uniqueElements<T>(listas: T[][]): T[] {
 export function capitalize(s: string): string {
   return s.charAt(0).toUpperCase() + s.slice(1);
 }
+
+export function dedent(
+  strings: TemplateStringsArray,
+  ...values: any[]
+): string {
+  const rawString = strings.reduce((result, str, i) => {
+    return `${result}${str}${values || ""}`;
+  }, "");
+
+  const lines = rawString.split("\n");
+  const nonEmptyLines = lines.filter((line) => line.trim() !== "");
+
+  if (nonEmptyLines.length === 0) {
+    return "";
+  }
+
+  const indentSize = Math.min(
+    ...nonEmptyLines.map((line) => line.match(/^\s*/)?.[0].length || 0)
+  );
+
+  return lines
+    .map((line) =>
+      line.startsWith(" ".repeat(indentSize)) ? line.slice(indentSize) : line
+    )
+    .join("\n")
+    .trim();
+}
--- tests/modules/pydantic-code-generator.test.ts
+++ tests/modules/pydantic-code-generator.test.ts
@@ -4,6 +4,7 @@ import {
   mergeClasses,
   mergeTypes
 } from "../../src/modules/pydantic-code-generator/pydantic-code-generator.module";
+import { dedent } from "../../src/modules/utils/utils.module";
 
 describe("pydantic-code-generator", () => {
   describe("generatePydanticCode", () => {
@@ -16,18 +17,20 @@ describe("pydantic-code-generator", () => {
           street: "Wall Street"
         }
       };
-      const expected = `from pydantic import BaseModel
+      const expected = dedent`
+        from pydantic import BaseModel
 
 
-class Address(BaseModel):
-  city: str
-  street: str
+        class Address(BaseModel):
+          city: str
+          street: str
 
 
-class Model(BaseModel):
-  name: str
-  age: int
-  address: Address`;
+        class Model(BaseModel):
+          name: str
+          age: int
+          address: Address
+      `;
 
       const result = generatePydanticCode(json);
 
@@ -54,32 +57,34 @@ class Model(BaseModel):
         }
       };
 
-      const expected = `from pydantic import BaseModel
+      const expected = dedent`
+        from pydantic import BaseModel
 
-from typing import List
-
-
-class Sort(BaseModel):
-  direction: str
+        from typing import List
 
 
-class Content(BaseModel):
-  id: int
-  sort: Sort
+        class Sort(BaseModel):
+          direction: str
 
 
-class Sort1(BaseModel):
-  sorted: bool
+        class Content(BaseModel):
+          id: int
+          sort: Sort
 
 
-class Pageable(BaseModel):
-  sort: Sort1
+        class Sort1(BaseModel):
+          sorted: bool
 
 
-class Model(BaseModel):
-  content: List[Content]
-  pageable: Pageable
-  sort: Sort1`;
+        class Pageable(BaseModel):
+          sort: Sort1
+
+
+        class Model(BaseModel):
+          content: List[Content]
+          pageable: Pageable
+          sort: Sort1
+      `;
 
       const result = generatePydanticCode(json);
 
@@ -114,32 +119,34 @@ class Model(BaseModel):
         ]
       };
 
-      const expected = `from pydantic import BaseModel
+      const expected = dedent`
+        from pydantic import BaseModel
 
-from typing import List
-
-
-class Sort(BaseModel):
-  direction: str
+        from typing import List
 
 
-class Content(BaseModel):
-  id: int
-  sort: List[Sort]
+        class Sort(BaseModel):
+          direction: str
 
 
-class Sort1(BaseModel):
-  sorted: bool
+        class Content(BaseModel):
+          id: int
+          sort: List[Sort]
 
 
-class Pageable(BaseModel):
-  sort: List[Sort1]
+        class Sort1(BaseModel):
+          sorted: bool
 
 
-class Model(BaseModel):
-  content: List[Content]
-  pageable: List[Pageable]
-  sort: List[Sort1]`;
+        class Pageable(BaseModel):
+          sort: List[Sort1]
+
+
+        class Model(BaseModel):
+          content: List[Content]
+          pageable: List[Pageable]
+          sort: List[Sort1]
+      `;
 
       const result = generatePydanticCode(json);
 
